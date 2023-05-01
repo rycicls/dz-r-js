086bccadcb225c42eae77fc3adeca3d5167183e0
rycicls <krists.mil@gmail.com>
2023-05-01 15:47:29 +0200

    added notes for code and made minor additions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from player_count import players_num
 
 symbols = ("clubs", "spades", "hearts", "diamonds")
 numbers = (2,3,4,5,6,7,8,9,10,"J","Q","K","A")
-jack_value = 11
+jack_value = 11 #values for cards others will look at number
 queen_value = 12
 king_value = 13
 ace_value = 14
@@ -13,7 +13,7 @@ class Card:
         self.symbol = cardSymbol
         self.number = cardNumber
 
-player_count = players_num()
+player_count = players_num() #gonna be visual curently asks how many players with cmd
 
 class Player:
     def __init__(self,name):
@@ -22,17 +22,21 @@ class Player:
 class Deck:
     def __init__(self):
         self.deck_list = []
+
+    def make_deck(self):
         for symbol in symbols:
             for number in numbers:
                 self.deck_list.append(Card(symbol, number))
+        return self.deck_list(symbol,number) # makes deck but doesn't show cards
+        
     
     def shufel_deck(self):
         self.shufeled_deck = random.shuffle(self.deck_list)
-        print(self.shufeled_deck)
+        return self.shufeled_deck #returns none
 
     def deal_decks(self):
-        self.player1_deck = []
+        self.player1_deck = [] #haven't started haven't figured out yet what to do
 
-sd = Deck
-sd.shufel_deck
-print(f"shuffeled deck: {sd.shufel_deck}")
+sd = Deck()
+print(f"deck: {sd.make_deck()}") #testing
+print(f"shuffeled deck: {sd.shufel_deck()}")
--- player_count.py
+++ player_count.py
@@ -1,4 +1,4 @@
-def players_num():
+def players_num(): #plan to make visual with pysimpleGUI
     print("how many players: ")
     player_count_input = input()
     player_count_input = int(player_count_input)
