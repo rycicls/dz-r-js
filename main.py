c68687c4e9b10fc06b0b694ebcd8f669df0600c3
rycicls <krists.mil@gmail.com>
2023-05-08 09:48:52 +0200

    so yeah I got confused by the main file and made a one so that I can understand it I don't know if it works and I will copy it to main when I am done

--- main.py
+++ main.py
@@ -18,7 +18,19 @@ player_count = players_num() #gonna be visual curently asks how many players wit
 class Player:
     def __init__(self,player_name):
         self.player_name = player_name
-        self.card_count = 52 / player_count
+        self.card_count = 52 // player_count
+        self.curr_player = 0
+        self.next_player = self.curr_player + 1
+        self.players = []
+        self.player_cards = []
+        for i in range(player_count):
+            self.players.append(Player(i))
+
+    def player_decks(self):
+        for self.players in player_count:
+            for c in range(self.card_count):
+                self.player_cards.append(sd.shufel_deck(c))
+                c += 1
 
 class Deck:
     def __init__(self):
@@ -28,9 +40,7 @@ class Deck:
         for symbol in symbols:
             for number in numbers:
                 self.deck_list.append(Card(symbol,number))
-        # return self.deck_list()
         
-    
     def shufel_deck(self):
         random.shuffle(self.deck_list)
         return self.deck_list #returns none
@@ -39,28 +49,17 @@ class Deck:
         for card in deck:
             print(f'symbol: {card.symbol}')
             print(f'value: {card.number}')
-    
-    def player_decks(self):
-        self.player_cards = []
-        for players in player_count:
-            for c in range(self.cards_per_deck):
-                self.player_cards.append(sd.shufel_deck(c))
-                c += 1
 
 class Game:
-    def players_names(self):
-        self.players = []
-        for i in range(player_count):
-            self.players.append(Player(i))
+    def __init__(self):
+        pass
 
     def move(self):
-        self.curr_player = 0
-        self.next_player = self.curr_player + 1
         print(f'to make move type a')
         move_input = input()
         move_input = str(move_input)
         if move_input == 'a':
-            # if sd.player_cards[0]
+            if pl.player_cards(0)
             pass
 
         if self.curr_player < self.player_count - 1:
@@ -74,7 +73,7 @@ class Game:
             self.next_player = 0
 
 gm = Game()
-# pl = Player()
+pl = Player()
 sd = Deck()
 print(f"shuffeled deck: {sd.print_deck(sd.shufel_deck())}")
-print(f"player name list: {gm.players_names()}")
+# print(f"player name list: {gm.players()}")
